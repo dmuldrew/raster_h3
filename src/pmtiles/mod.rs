c9cdf17647c3cb609efbdc0b6e3955cb25f1e92f
dmuldrew <dmuldrew@gmail.com>
2026-09-12 07:56:11 -0700

    refactor(pmtiles): extract Parquet-to-PMTiles transcoding into parquet_tiler module

--- src/pmtiles/mod.rs
+++ src/pmtiles/mod.rs
@@ -5,12 +5,14 @@
 
 pub mod features;
 pub mod mvt;
+pub mod parquet_tiler;
 pub mod pyramid;
 pub mod tiler;
 pub mod writer;
 
 pub use features::{H3Feature, PmtilesExportSummary, ResolutionAccumulatorStats};
 pub use mvt::{FeatureProperties, MercatorPoint, MvtFeature, MvtLayer, MvtValue};
+pub use parquet_tiler::{process_parquet_to_pmtiles, RowGroupExtent};
 pub use pyramid::{
     cell_boundary_mercator, cell_tile_range, cell_tile_range_mercator, h3_res_for_zoom,
     h3_res_to_zoom, lon_lat_to_tile_xy, max_hex_radius_deg, mercator_to_tile_xy,
@@ -18,3 +20,4 @@ pub use pyramid::{
 };
 pub use tiler::H3PmtilesTiler;
 pub use writer::{zxy_to_tile_id, PmtilesWriter, TilePayload};
+
